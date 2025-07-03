7d432b3c004619ee4a5092c93aeeb15d0604963f
DiegoGabrielLL <diego.lazzarin@escola.pr.gov.br>
2025-07-03 18:01:23 -0300

    Update criando-tabelas.sql

--- criando-tabelas.sql
+++ criando-tabelas.sql
@@ -1,10 +1,11 @@
-CREATE TABLE usuario (
-    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    senha VARCHAR(255) NOT NULL,
-    primeiro_nome VARCHAR(50) NOT NULL,
-    sobrenome VARCHAR(50) NOT NULL,
-    telefone VARCHAR(20),
-    id_cliente INT,
-    endereco VARCHAR(255) 
-    );
+use ***;
+CREATE TABLE users (
+  id_usuario int DEFAULT NULL auto_increment,
+  email varchar(100) NOT NULL,
+  senha varchar(255) NOT NULL,
+  primeiro_nome varchar(50) NOT NULL,
+  sobrenome varchar(50) NOT NULL,
+  telefone varchar(20) NOT NULL,
+  endereco varchar(225) NOT NULL,
+  primary key (id_usuario)
+)
